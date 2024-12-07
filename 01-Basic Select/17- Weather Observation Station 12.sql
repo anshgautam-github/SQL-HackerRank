9824f36703a90a675b0d0bd5603b00e081c9bcab
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2024-12-07 17:21:47 +0530

    Create 17- Weather Observation Station 12.sql

---
+++ 01-Basic Select/17- Weather Observation Station 12.sql
@@ -0,0 +1,29 @@
+/*
+Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+
+Input Format--
+The STATION table is described as follows:
+*/
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LOWER(CITY) NOT LIKE 'a%' 
+  AND LOWER(CITY) NOT LIKE 'e%' 
+  AND LOWER(CITY) NOT LIKE 'i%' 
+  AND LOWER(CITY) NOT LIKE 'o%' 
+  AND LOWER(CITY) NOT LIKE 'u%' 
+  AND LOWER(CITY) NOT LIKE '%a'
+  AND LOWER(CITY) NOT LIKE '%e'
+  AND LOWER(CITY) NOT LIKE '%i'
+  AND LOWER(CITY) NOT LIKE '%o'
+  AND LOWER(CITY) NOT LIKE '%u';
+
+
+/*
+Alternate solution -> 
+
+SELECT DISTINCT CITY FROM STATION 
+                  WHERE NOT((CITY LIKE 'a%' OR CITY LIKE 'e%' OR CITY LIKE 'i%' OR CITY LIKE 'o%' OR CITY LIKE 'u%') 
+                    OR
+                  (CITY LIKE '%a' OR CITY LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o' OR CITY LIKE '%u'));
+*/
