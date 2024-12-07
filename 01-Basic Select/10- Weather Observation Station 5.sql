d756a0bd131d3fccf76a414e33b879a4e1117dac
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2024-12-07 15:00:43 +0530

    Update 10- Weather Observation Station 5.sql

--- 01-Basic Select/10- Weather Observation Station 5.sql
+++ 01-Basic Select/10- Weather Observation Station 5.sql
@@ -5,3 +5,12 @@ The STATION table is described as follows:
 
 SELECT CITY, LENGTH(CITY) from STATION ORDER BY LENGTH(CITY), CITY LIMIT 1;
 SELECT CITY, LENGTH(CITY) from STATION ORDER BY LENGTH(CITY) DESC, CITY LIMIT 1;
+
+
+/*
+The MIN() and MAX() Functions in SQL->> 
+You might think we can use MAX and MIN function 
+These functions are generally used to operate on numeric or comparable values, such as integers or dates. 
+When you try to use them directly on string lengths, they will return the smallest or largest length, 
+not the actual city name associated with that length.
+*/
