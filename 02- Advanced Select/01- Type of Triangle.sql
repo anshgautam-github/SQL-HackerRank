c78eceee7f9b86fe900077d7b6c5120d34da10a7
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2024-12-08 22:35:58 +0530

    Update 01- Type of Triangle.sql

--- 02- Advanced Select/01- Type of Triangle.sql
+++ 02- Advanced Select/01- Type of Triangle.sql
@@ -10,3 +10,45 @@ Input Format
 The TRIANGLES table is described as follows:
 
 */
+
+
+
+
+
+
+/*
+
+-------------------------The CASE Statement in SQL ------------------------------
+
+CASE is a conditional statement in SQL. It’s like if-else in programming.
+It evaluates conditions in order and returns the result of the first true condition.
+Use CASE to check multiple conditions in order.
+
+--------------------------------CASE syntax --------------------------
+
+CASE
+    WHEN condition1 THEN result1
+    WHEN condition2 THEN result2
+    ELSE result3
+END
+
+---------------------SELECT limiations----------------------------
+The limitation of the SELECT statement that leads us to use CASE is its lack of built-in conditional logic for dynamic categorization. 
+Without CASE, SQL lacks a direct way to:
+
+1- Apply Multiple Conditions Dynamically:
+A standard SELECT retrieves columns or calculated values directly but cannot change the output based on dynamic, condition-driven rules.
+Assign Custom Labels Based on Conditions:
+
+2- Assign Custom Labels Based on Conditions:
+If you want to assign labels like "Equilateral" or "Scalene" based on the values of columns 
+𝐴,B, and C, you can't do this with basic SELECT alone.
+
+3- Avoid Repetition of Logic:
+Without CASE, you would need multiple SELECT statements or complex WHERE clauses to implement conditional logic,
+resulting in redundancy and reduced readability.
+
+
+
+
+*/
