ad252f511d218fbb7cdfbe2f73770e761d730fb3
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2024-12-25 23:44:50 +0530

    Update 06- Population Density Difference.sql

--- 03- Aggregation/06- Population Density Difference.sql
+++ 03- Aggregation/06- Population Density Difference.sql
@@ -4,3 +4,5 @@ Query the difference between the maximum and minimum populations in CITY.
 Input Format:
 The CITY table is described as follows: 
 */
+
+select (max(population)-min(population)) from city;
